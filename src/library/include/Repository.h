e9bb96f5face80074d66d0b69171fd81e27aba65
Tomasz Witczak <TomaszMWitczak@gmail.com>
2018-12-29 04:21:25 +0100

    Add Repository content without implementation

--- src/library/include/Repository.h
+++ src/library/include/Repository.h
@@ -1,10 +1,23 @@
 #ifndef RESTAURANT_REPOSITORY_H
 #define RESTAURANT_REPOSITORY_H
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+#include <string>
+#include <vector>
+//--------------------------------------------------------------------------------------------------
 namespace Restaurant
 {
+	template <typename T>
 	class Repository
 	{
+	public:
+		bool add(T element);
+		void remove(T element);
+		void saveToFile(std::string filename) const;
+		void readFromFile(std::string filename);
+		std::vector<T> getAll() const;
+
+	protected:
+		std::vector<T> elements;
 	};
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
