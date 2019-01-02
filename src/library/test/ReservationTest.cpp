a3da18257c69d3d7b53140fd82d9a4aaff33921a
Tomasz Witczak <TomaszMWitczak@gmail.com>
2019-01-02 23:14:48 +0100

    Add several Reservation tests

---
+++ src/library/test/ReservationTest.cpp
@@ -0,0 +1,65 @@
+#include <boost/test/unit_test.hpp>
+////////////////////////////////////////////////////////////////////////////////////////////////////
+#include "Reservation.h"
+#include "Client.h"
+#include "Table.h"
+//--------------------------------------------------------------------------------------------------
+template <typename T>
+bool compareVectors(std::vector<T> const &container1, std::vector<T> const &container2)
+{
+	return std::equal(container1.begin(), container1.begin(), container2.begin());
+}
+std::tuple<Restaurant::Client_Ptr, std::vector<Restaurant::Table_Ptr>, int, int> getReservationParameters()
+{
+	return	{	std::make_shared<Restaurant::Client>(
+						Restaurant::Client("John", "Smith", "123456789", "basic")),
+		  		{ std::make_shared<Restaurant::Table>(Restaurant::Table(2)) },
+				2,
+				4
+			};
+}
+//--------------------------------------------------------------------------------------------------
+BOOST_AUTO_TEST_SUITE(Reservation_CoreFunctionality_TestSuite)
+	BOOST_AUTO_TEST_CASE(Reservation_ConstructAndGetClient_TestCase)
+	{
+		auto const [client1, tables, beginTime, endTime] = getReservationParameters();
+
+		Restaurant::Reservation reservation(client1, tables, beginTime, endTime);
+		Restaurant::Client_Ptr const client2 = reservation.getClient();
+
+		BOOST_REQUIRE_EQUAL(client1, client2);
+	}
+	BOOST_AUTO_TEST_CASE(Reservation_ConstructAndGetTables_TestCase)
+	{
+		auto const [client, tables1, beginTime, endTime] = getReservationParameters();
+
+		Restaurant::Reservation reservation(client, tables1, beginTime, endTime);
+		std::vector<Restaurant::Table_Ptr> const tables2 = reservation.getTables();
+
+		BOOST_REQUIRE_EQUAL(compareVectors(tables1, tables2), true);
+	}
+	BOOST_AUTO_TEST_CASE(Reservation_ConstructAndGetID_TestCase)
+	{
+		// TODO: Is this test really needed?
+	}
+	BOOST_AUTO_TEST_CASE(Reservation_ConstructAndGetBeginTime_TestCase)
+	{
+		auto const [client, tables, beginTime1, endTime] = getReservationParameters();
+
+		Restaurant::Reservation reservation(client, tables, beginTime1, endTime);
+		int const beginTime2 = reservation.getBeginTime();
+
+		BOOST_REQUIRE_EQUAL(beginTime1, beginTime2);
+	}
+	BOOST_AUTO_TEST_CASE(Reservation_ConstructAndGetEndTime_TestCase)
+	{
+		auto const [client, tables, beginTime, endTime1] = getReservationParameters();
+
+		Restaurant::Reservation reservation(client, tables, beginTime, endTime1);
+		int const endTime2 = reservation.getEndTime();
+
+		BOOST_REQUIRE_EQUAL(endTime1, endTime2);
+	}
+	// TODO: Write tests for exceptions in constructor
+BOOST_AUTO_TEST_SUITE_END()
+////////////////////////////////////////////////////////////////////////////////////////////////////
