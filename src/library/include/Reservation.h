8eae85a28618dfca85417b9ad31a85e5463ed9f3
Tomasz Witczak <TomaszMWitczak@gmail.com>
2019-01-02 23:17:30 +0100

    Add Reservation class

--- src/library/include/Reservation.h
+++ src/library/include/Reservation.h
@@ -1,10 +1,38 @@
 #ifndef RESTAURANT_RESERVATION_H
 #define RESTAURANT_RESERVATION_H
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+#include <memory>
+#include <vector>
+
+namespace Restaurant
+{
+	class Client;
+	class Table;
+
+	typedef std::shared_ptr<Client> Client_Ptr;
+	typedef std::shared_ptr<Table> Table_Ptr;
+}
+//--------------------------------------------------------------------------------------------------
 namespace Restaurant
 {
 	class Reservation
 	{
+	public:
+		Reservation(Client_Ptr const &client, std::vector<Table_Ptr> const &tables,
+					int const &beginTime, int const &endTime);
+
+		Client_Ptr 				getClient() 	const;
+		std::vector<Table_Ptr> 	getTables() 	const;
+		int 					getID() 		const;
+		int 					getBeginTime() 	const;
+		int 					getEndTime() 	const;
+
+	private:
+		Client_Ptr 				const client;
+		std::vector<Table_Ptr> 	const tables;
+		int 					const uuid; 		// TODO: Replace int with UUID
+		int 					const beginTime; 	// TODO: Replace int with localDateTime
+		int 					const endTime; 		// TODO: Replace int with localDateTime
 	};
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
--- src/library/src/Reservation.cpp
+++ src/library/src/Reservation.cpp
@@ -1,3 +1,38 @@
 #include "Reservation.h"
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+Restaurant::Reservation::Reservation(Restaurant::Client_Ptr const &client,
+									 std::vector<Restaurant::Table_Ptr> const &tables,
+									 int const &beginTime, int const &endTime)
+ 	:	client(client),
+ 		tables(tables),
+ 		uuid(0), //TODO
+ 		beginTime(beginTime),
+ 		endTime(endTime)
+{
+}
+//--------------------------------------------------------------------------------------------------
+Restaurant::Client_Ptr Restaurant::Reservation::getClient() const
+{
+	return client;
+}
+//--------------------------------------------------------------------------------------------------
+std::vector<Restaurant::Table_Ptr> Restaurant::Reservation::getTables() const
+{
+	return tables;
+}
+//--------------------------------------------------------------------------------------------------
+int Restaurant::Reservation::getID() const
+{
+	return uuid;
+}
+//--------------------------------------------------------------------------------------------------
+int Restaurant::Reservation::getBeginTime() const
+{
+	return beginTime;
+}
+//--------------------------------------------------------------------------------------------------
+int Restaurant::Reservation::getEndTime() const
+{
+	return endTime;
+}
 ////////////////////////////////////////////////////////////////////////////////////////////////////
