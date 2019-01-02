dd171dec117b5e9caf817fe1dffae1b9b637b8bc
Tomasz Witczak <TomaszMWitczak@gmail.com>
2019-01-02 23:18:35 +0100

    Add several methods to Client and ClientRepository

--- src/library/include/Client.h
+++ src/library/include/Client.h
@@ -1,10 +1,19 @@
 #ifndef RESTAURANT_CLIENT_H
 #define RESTAURANT_CLIENT_H
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+#include <string>
+//--------------------------------------------------------------------------------------------------
 namespace Restaurant
 {
 	class Client
 	{
+	public:
+		Client(std::string const &firstName, std::string const &lastName,
+				std::string const &telephoneNumber, std::string const &clientType);
+	private:
+		std::string const firstName,
+						  lastName,
+						  telephoneNumber;
 	};
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
--- src/library/include/ClientRepository.h
+++ src/library/include/ClientRepository.h
@@ -1,10 +1,21 @@
 #ifndef RESTAURANT_CLIENTREPOSITORY_H
 #define RESTAURANT_CLIENTREPOSITORY_H
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+//#include "Client.h"
+#include "Repository.h"
+#include <memory>
+//--------------------------------------------------------------------------------------------------
 namespace Restaurant
 {
+	class Client;//#include "Client.h"
+
 	class ClientRepository
+			: public Repository<std::shared_ptr<Client>>
 	{
+	public:
+		std::vector<std::shared_ptr<Client>> findClientsByFirstName(std::string firstName) const;
+		std::vector<std::shared_ptr<Client>> findClientsByLastName(std::string lastName) const;
+		std::vector<std::shared_ptr<Client>> findClientsByTelephoneNumber(std::string telephoneNumber) const;
 	};
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
