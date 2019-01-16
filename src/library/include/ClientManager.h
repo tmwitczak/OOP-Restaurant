3b9f4ce9b7c777242b9d3196dc2f2614deb1bc15
Tomasz Witczak <TomaszMWitczak@gmail.com>
2019-01-16 01:39:15 +0100

    Change code formatting

--- src/library/include/ClientManager.h
+++ src/library/include/ClientManager.h
@@ -1,37 +1,42 @@
 #ifndef RESTAURANT_CLIENTMANAGER_H
 #define RESTAURANT_CLIENTMANAGER_H
-////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////// Included headers
 #include <boost/date_time/local_time/local_date_time.hpp>
 #include "ClientRepository.h"
 
 namespace Restaurant
 {
+	/////////////////////////////////////////////////////////////////////////// Forward declarations
 	class Client;
-	//class Table;
 
+	/////////////////////////////////////////////////////////////////////////////////////// Typedefs
 	typedef std::shared_ptr<Client> Client_Ptr;
-	//typedef std::shared_ptr<Table> Table_Ptr;
 
-	//typedef std::shared_ptr<boost::local_time::local_date_time> DateTime_Ptr;
-}
-//--------------------------------------------------------------------------------------------------
-namespace Restaurant
-{
+	/////////////////////////////////////////////////////////////////////////////// Class definition
 	class ClientManager
 	{
+	//---------------------------------------------------------------------------------- [ Methods ]
 	public:
+		//------------------------------------------------------------ Client creation and removal <
 		Client_Ptr makeClient(std::string const &firstName, std::string const &lastName,
 							  std::string const &telephoneNumber, std::string const &clientType);
 		void removeClient(Client_Ptr const &client);
+
+		//------------------------------------------------------------------------- Search methods <
 		std::vector<Client_Ptr> findClientsByFirstName(std::string const &firstName) const;
 		std::vector<Client_Ptr> findClientsByLastName(std::string const &lastName) const;
 		std::vector<Client_Ptr> findClientsByTelephoneNumber(
 				std::string const &telephoneNumber) const;
+
+		//------------------------------------------------------------------------ File operations <
 		void saveClientsToFile(std::string const &filename) const;
 		void readClientsFromFile(std::string const &filename);
+
+		//-------------------------------------------------------------------------------- Getters <
 		std::vector<Client_Ptr> getAllClients() const;
 		std::string getInfo() const;
 
+	//----------------------------------------------------------------------------------- [ Fields ]
 	private:
 		ClientRepository clientRepository;
 	};
