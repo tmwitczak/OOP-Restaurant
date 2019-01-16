3feedccfee0993c1de52927bce57d89bb77adbe5
kjawo <konjaworski@gmail.com>
2019-01-16 15:39:13 +0100

    Add several methods to Reservation Manager and Repository

--- src/library/include/ReservationRepository.h
+++ src/library/include/ReservationRepository.h
@@ -1,10 +1,31 @@
 #ifndef RESTAURANT_RESERVATIONREPOSITORY_H
 #define RESTAURANT_RESERVATIONREPOSITORY_H
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+#include "Repository.h"
+#include <memory>
+#include <boost/uuid/uuid.hpp>
+
+namespace Restaurant
+{
+	class Reservation;
+	class Client;
+	class Table;
+
+	typedef std::shared_ptr<Reservation> Reservation_Ptr;
+	typedef std::shared_ptr<Client> Client_Ptr;
+	typedef std::shared_ptr<Table> Table_Ptr;
+}
+
 namespace Restaurant
 {
 	class ReservationRepository
+			: public  Repository<Reservation_Ptr>
 	{
+	public:
+		Reservation_Ptr 				findReservationByID(boost::uuids::uuid id);
+		std::vector<Reservation_Ptr> 	findReservationByClient(Client_Ptr client);
+		std::vector<Reservation_Ptr> 	findReservationByTable(Table_Ptr table);
+
 	};
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
--- src/library/src/ReservationManager.cpp
+++ src/library/src/ReservationManager.cpp
@@ -1,3 +1,15 @@
 #include "ReservationManager.h"
+#include "Reservation.h"
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+/*Restaurant::Reservation_Ptr
+Restaurant::ReservationManager::makeReservation(const Restaurant::Client_Ptr &client,
+                                                const Restaurant::Table_Ptr &table,
+                                                const Restaurant::DateTime_Ptr &beginTime,
+                                                const Restaurant::DateTime_Ptr &endTime)
+{
+    Restaurant::Reservation_Ptr reservation = std::make_shared<Restaurant::Reservation>(client, table,
+            beginTime, endTime);
+    reservationRepository.add(reservation);
+    return reservation;
+}*/
